cb7e84456c44b75f8ef9fa5efc4468e0dfb51d8a
p1gt <ranaranap59@gmail.com>
2023-07-09 16:45:57 +0300

    Devices

--- OOP/Devices/src/Phone.java
+++ OOP/Devices/src/Phone.java
@@ -1,12 +1,20 @@
 public class Phone extends Device {
     public void call() {
-        battery -= 5;
-        System.out.printf("making a call...\nremaining battery: %d\n", battery);
+        if (battery < 10) {
+            System.out.println("battery is too low, please charge your phone");
+        } else {
+            battery -= 5;
+            System.out.printf("making a call...\nremaining battery: %d\n", battery);
+        }
     }
 
     public void play() {
-        battery -= 20;
-        System.out.printf("playing a game...\nremaining battery: %d\n", battery);
+        if (battery < 25) {
+            System.out.println("battery is too low, please charge your phone");
+        } else {
+            battery -= 20;
+            System.out.printf("playing a game...\nremaining battery: %d\n", battery);
+        }
     }
 
     public void charge() {
