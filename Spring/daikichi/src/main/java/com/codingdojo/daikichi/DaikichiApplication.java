b8e032862d8c45e8ebab3383d6344ea187ad3d0e
p1gt <ranaranap59@gmail.com>
2023-07-12 14:39:41 +0300

    Daikichi routes

--- Spring/daikichi/src/main/java/com/codingdojo/daikichi/DaikichiApplication.java
+++ Spring/daikichi/src/main/java/com/codingdojo/daikichi/DaikichiApplication.java
@@ -2,30 +2,11 @@ package com.codingdojo.daikichi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-@RequestMapping("/daikichi")
 public class DaikichiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DaikichiApplication.class, args);
-    }
-
-    @RequestMapping("")
-    public String root() {
-        return "Welcome!";
-    }
-
-    @RequestMapping("today")
-    public String today() {
-        return "today you will find luck in all your endeavors!";
-    }
-
-    @RequestMapping("tomorrow")
-    public String tomorrow() {
-        return "tomorrow an opportunity will arise, so be sure to be open to new ideas";
     }
 }
