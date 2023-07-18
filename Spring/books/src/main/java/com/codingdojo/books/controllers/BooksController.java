baca40f0a9edb8a0fc411f527cfa8fff8d62f93b
p1gt <ranaranap59@gmail.com>
2023-07-18 18:29:34 +0300

    Books

---
+++ Spring/books/src/main/java/com/codingdojo/books/controllers/BooksController.java
@@ -0,0 +1,23 @@
+package com.codingdojo.books.controllers;
+
+import com.codingdojo.books.models.Book;
+import com.codingdojo.books.services.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class BooksController {
+    private final BookService service;
+
+    public BooksController(BookService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/books/{id}")
+    public String read(@PathVariable("id") Long id, Model model) {
+        Book book = service.findBook(id);
+        model.addAttribute("book", book);
+        return "show.jsp";
+    }
+}
---
+++ Spring/books/src/main/webapp/WEB-INF/show.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: odayi
+  Date: 7/18/2023
+  Time: 5:41 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+	<title><c:out value="${book.id}"/></title>
+</head>
+<body>
+	<a></a>
+	<h1><c:out value="${book.title}"/></h1>
+	<p>description: <c:out value="${book.description}"/></p>
+	<p>language: <c:out value="${book.language}"/></p>
+	<p>pages: <c:out value="${book.numberOfPages}"/></p>
+</body>
+</html>
